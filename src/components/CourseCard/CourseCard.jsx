20c0e6ccd2778ddfdf7df68927c2bae15780fad3
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-05-17 16:28:18 -0700

    fix to toggle Faith and Nova

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -1,34 +1,19 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { useState } from "react";
 
-import React from 'react'
-import Card from 'react-bootstrap/Card'
-import { useState } from 'react'
-
-export default function CourseCard (props) {
-
+export default function CourseCard(props) {
   const [isActive, setIsActive] = useState(false);
 
   const toggleDiffStyle = () => {
     setIsActive(!isActive);
-  }
+  };
 
   return (
     <>
-    <button onClick={toggleDiffStyle}>Click Me</button>
-      <Card style={{ width: '18rem' }}>
+      <button onClick={toggleDiffStyle}>Click Me</button>
+      <Card style={{ width: "18rem" }}>
         <Card.Body id={`${isActive ? "diffStyle" : "null"}`}>
-
-import React from "react";
-import Card from "react-bootstrap/Card";
-
-export default function CourseCard(props) {
-  return (
-    <>
-      <Card
-        style={{ width: "18rem" }}
-        className={`font${props.isFontActive ? "largeFont" : "null"}`}
-      >
-        <Card.Body>
-
           <Card.Title>{props.courseData.courseName}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
             {props.courseData.instructorName}
