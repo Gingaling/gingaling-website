2302ce44560a2db78e20e2d9cd7f178c3940e849
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-05-17 14:33:33 -0700

    ternary to toggle classes

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card'
+import React from "react";
+import Card from "react-bootstrap/Card";
 
-export default function CourseCard (props) {
+export default function CourseCard(props) {
   return (
     <>
-      <Card style={{ width: '18rem' }}>
+      <Card
+        style={{ width: "18rem" }}
+        className={`font${props.isFontActive ? "largeFont" : "null"}`}
+      >
         <Card.Body>
           <Card.Title>{props.courseData.courseName}</Card.Title>
-          <Card.Subtitle className='mb-2 text-muted'>
+          <Card.Subtitle className="mb-2 text-muted">
             {props.courseData.instructorName}
           </Card.Subtitle>
           <Card.Text>{props.courseData.courseDescription}</Card.Text>
-          <Card.Link href='#'>Link to Details Page</Card.Link>
+          <Card.Link href="#">Link to Details Page</Card.Link>
         </Card.Body>
       </Card>
     </>
-  )
+  );
 }
---
+++ src/components/ToggleButton/ToggleButton.js
@@ -0,0 +1,19 @@
+export default function ButtonToggle(props) {
+  return (
+    <>
+      <div className="container">
+        <div className={`font${props.isFontActive ? "largeFont" : "null"}`}>
+          <button onClick={props.toogleFont}>FONT SIZE</button>
+        </div>
+        <br></br>
+        {/* <div className={`color${isActive ? "colorblind" : "null"}`}>
+          <button onClick={toggleClass}>COLOR</button>
+        </div>
+        <br></br>
+        <div className={`day${isActive ? "night" : "null"}`}>
+          <button onClick={toggleClass}>NIGHT VERSION</button>
+        </div> */}
+      </div>
+    </>
+  );
+}
