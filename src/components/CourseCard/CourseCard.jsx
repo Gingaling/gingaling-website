1a14c276d86e6efa70d92127f3d2119c239c368c
Gingaling <nycgingsf@hotmail.com>
2022-05-17 16:11:05 -0700

    Committing toggle files

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
+import { useState } from 'react'
 
 export default function CourseCard (props) {
+
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleDiffStyle = () => {
+    setIsActive(!isActive);
+  }
+
   return (
     <>
+    <button onClick={toggleDiffStyle}>Click Me</button>
       <Card style={{ width: '18rem' }}>
-        <Card.Body>
+        <Card.Body id={`${isActive ? "diffStyle" : "null"}`}>
           <Card.Title>{props.courseData.courseName}</Card.Title>
           <Card.Subtitle className='mb-2 text-muted'>
             {props.courseData.instructorName}
