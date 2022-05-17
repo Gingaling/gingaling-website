4a0637c7c167c09ca180544e5f4ef402e0eb558c
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-05-17 13:59:53 -0700

    change links to syllabus

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,42 +1,54 @@
-import React from 'react'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
-import { Link, NavLink } from 'react-router-dom'
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { Link, NavLink } from "react-router-dom";
 
 const authenticatedOptions = (
   <>
-    <NavLink to='/change-password' className='nav-link'>Change Password</NavLink>
-    <NavLink to='/sign-out' className='nav-link'>Sign Out</NavLink>
+    <NavLink to="/change-password" className="nav-link">
+      Change Password
+    </NavLink>
+    <NavLink to="/sign-out" className="nav-link">
+      Sign Out
+    </NavLink>
     {/* <NavLink to='/' className='nav-link'>Add a Syllabus</NavLink> */}
   </>
-)
+);
 
 const unauthenticatedOptions = (
   <>
-    <NavLink to='/sign-up' className='nav-link'>Sign Up</NavLink>
-    <NavLink to='/sign-in' className='nav-link'>Sign In</NavLink>
+    <NavLink to="/sign-up" className="nav-link">
+      Sign Up
+    </NavLink>
+    <NavLink to="/sign-in" className="nav-link">
+      Sign In
+    </NavLink>
   </>
-)
+);
 
 const alwaysOptions = (
   <>
     {/* <NavLink to='/' className='nav-link'>Home</NavLink> */}
-    <NavLink to='/courses' className='nav-link'>Courses</NavLink>
+    <NavLink to="/syllabus" className="nav-link">
+      Syllabus
+    </NavLink>
   </>
-)
+);
 
 const Header = ({ user }) => (
-  <Navbar bg='primary' variant='dark' expand='md'>
+  <Navbar bg="primary" variant="dark" expand="md">
     <Container>
       <Navbar.Brand>
-        <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>react-auth-template</Link>
+        <Link to="/" style={{ color: "#FFF", textDecoration: "none" }}>
+          react-auth-template
+        </Link>
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls='basic-navbar-nav' />
-      <Navbar.Collapse id='basic-navbar-nav'>
-        <Nav className='ms-auto'>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
           {user && (
-            <span className='navbar-text me-2'>Welcome, {user.email}</span>
+            <span className="navbar-text me-2">Welcome, {user.email}</span>
           )}
           {alwaysOptions}
           {user ? authenticatedOptions : unauthenticatedOptions}
@@ -44,6 +56,6 @@ const Header = ({ user }) => (
       </Navbar.Collapse>
     </Container>
   </Navbar>
-)
+);
 
-export default Header
+export default Header;
